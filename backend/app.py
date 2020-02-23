b5e53942b4871ed4c3f1a5e03744c7b6adb252ab
JeongHaeMin <kjhm0607@gmail.com>
2020-02-23 23:47:29 +0900

    [feat] sqlalchemy mysql 연동

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,15 @@
+from os import path
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read(path.abspath(path.join(path.dirname(__file__), 'config.ini')))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = config['SQL_ALCHEMY']['URI']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 
