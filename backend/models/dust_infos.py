439cbbbcd79d99b70caf52c3b68132b3f3c0f873
JeongHaeMin <kjhm0607@gmail.com>
2020-02-23 23:46:55 +0900

    [fix] 미세먼지 모델 변경
    - init 삭제
    - date_time -> date format -> varchar

--- backend/models/dust_infos.py
+++ backend/models/dust_infos.py
@@ -1,13 +1,35 @@
 from backend.app import db
-from sqlalchemy import Column
+from datetime import datetime
 
 
 class DustInfos(db.Model):
     __tablename__ = 'dust_infos'
     __table_args__ = {'mysql_collate': 'utf8_general_ci'}
 
-    id = Column(db.Integer, nullable=False, primary_key=True, autoincrement=True)
-    city = Column(db.String(25), nullable=False)
-    pm2 = Column(db.String(10), nullable=True)
-    pm10 = Column(db.String(10), nullable=True)
-    date_time = Column(db.DateTime)
+    id = db.Column(db.Integer, nullable=False, primary_key=True, autoincrement=True)
+    city = db.Column(db.String(25), nullable=False)
+    pm2 = db.Column(db.String(10), nullable=True)
+    pm10 = db.Column(db.String(10), nullable=True)
+    date_time = db.Column(db.String(30), nullable=False)
+
+#     def __init__(self, id, city, pm2, pm10, date_time):
+#         self._id = id
+#         self._city = city
+#         self._pm2 = pm2 if pm2 else None
+#         self._pm10 = pm10 if pm10 else None
+#         self._date_time = date_time
+#
+#
+# def add(items):
+#     if not isinstance(items, list):
+#         items = [items]
+#
+#     for item in items:
+#         event = DustInfos(id=item.get('id'),
+#                           city=item.get('city'),
+#                           pm2=item.get('pm2'),
+#                           pm10=item.get('pm10'),
+#                           date_time=datetime.now().strftime('%Y%m%d'))
+#         db.session.add(event)
+#         db.session.commit()
+#
