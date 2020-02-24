fe22dc03e0745ed43402e65d01dc252988148a68
JeongHaeMin <kjhm0607@gmail.com>
2020-02-24 23:44:43 +0900

    [refactor] app 변경

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,11 @@
 from os import path
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from configparser import ConfigParser
+from backend import create_app
 
-config = ConfigParser()
-config.read(path.abspath(path.join(path.dirname(__file__), 'config.ini')))
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = config['SQL_ALCHEMY']['URI']
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
+app, db = create_app()
 
 @app.route('/', methods=['GET'])
 def index():
-    return 'welcome public open api server'
+    return 'index page'
 
 
 if __name__ == '__main__':
