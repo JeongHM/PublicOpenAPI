040105d28ab01ce5ece0c91bb90381ee0333b954
JeongHaeMin <kjhm0607@gmail.com>
2020-02-25 23:50:41 +0900

    [feat] 현재 미세먼지 데이터 API 기능 Mock up

--- backend/api/dust.py
+++ backend/api/dust.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 dust_blueprint = Blueprint('dust', __name__, url_prefix='/api/dust')
 from backend.models.dust_infos import DustInfos
+from datetime import datetime
 
 
 @dust_blueprint.route('/', methods=['GET'])
@@ -10,5 +11,13 @@ def index():
 
 @dust_blueprint.route('/current/', methods=['GET'])
 def get_current_dust_info():
-    # ORM
-    return '123'
+    date_time = request.args.get('date_time')
+    if date_time:
+        pass
+    else:
+        # date_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        date_time = '2020-02-23 23:00'
+
+    rows = DustInfos.query.filter_by(date_time=date_time).all()
+    result = [row.serialize for row in rows]
+    return jsonify({'data': result})
