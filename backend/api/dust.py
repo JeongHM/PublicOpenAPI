60eb4896a5c2650e7e3905209fd484dc1aea9ef8
JeongHaeMin <kjhm0607@gmail.com>
2020-02-26 23:20:16 +0900

    [feat] response fomatting 데코레이터 추가
    - response 포맷팅
    - 데코레이터 작성

--- backend/api/dust.py
+++ backend/api/dust.py
@@ -1,8 +1,7 @@
 from flask import Blueprint, request, jsonify
 dust_blueprint = Blueprint('dust', __name__, url_prefix='/api/dust')
 from backend.models.dust_infos import DustInfos
-from datetime import datetime
-
+from backend.common.decorators import response_format_decorator
 
 @dust_blueprint.route('/', methods=['GET'])
 def index():
@@ -10,14 +9,14 @@ def index():
 
 
 @dust_blueprint.route('/current/', methods=['GET'])
+@response_format_decorator
 def get_current_dust_info():
     date_time = request.args.get('date_time')
     if date_time:
         pass
     else:
-        # date_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         date_time = '2020-02-23 23:00'
 
     rows = DustInfos.query.filter_by(date_time=date_time).all()
     result = [row.serialize for row in rows]
-    return jsonify({'data': result})
+    return {'code': 200, 'data': None}, result
