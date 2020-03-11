01e90eccc6bc919c70e17c5912759811425aeb90
JeongHaeMin <kjhm0607@gmail.com>
2020-03-11 21:29:08 +0900

    [feat] 선택 도시 미세먼지 API 구현

--- backend/controllers/api/dust.py
+++ backend/controllers/api/dust.py
@@ -2,6 +2,8 @@ from flask import Blueprint, request, jsonify
 dust_blueprint = Blueprint('dust', __name__, url_prefix='/api/dust')
 from backend.models.dust_infos import DustInfos
 from backend.common.decorators import response_format_decorator
+from backend.common.constants import DEFINED_DICT
+
 
 @dust_blueprint.route('/', methods=['GET'], endpoint='index')
 def index():
@@ -13,12 +15,28 @@ def index():
 def update_dust_info():
     headers = dict(request.headers)
     if headers.get('api-key') == "dkssudgktpdy":
-        pass
+        return DEFINED_DICT['SUCCESS_CODE'], None
     else:
         result = {'data': '잘못된 접근입니다'}
-        return {'code': 404, 'data': None}, result
+        return DEFINED_DICT['ERROR_CODE'], result
 
-@dust_blueprint.route('/current/', methods=['GET'], endpoint='get_current_dust_info')
+
+@dust_blueprint.route('/city', methods=['GET'], endpoint='get_city_dust_info')
+@response_format_decorator
+def get_city_dust_info():
+    try:
+        body = request.json
+        city = body.get('city')
+        if city:
+            pass
+
+    except Exception as e:
+        result = {'result': str(e)}
+        return DEFINED_DICT['ERROR_CODE'], result
+    return DEFINED_DICT['SUCCESS_CODE'], {'result': None}
+
+
+@dust_blueprint.route('/current', methods=['GET'], endpoint='get_current_dust_info')
 @response_format_decorator
 def get_current_dust_info():
     date_time = request.args.get('date_time')
@@ -29,4 +47,5 @@ def get_current_dust_info():
 
     rows = DustInfos.query.filter_by(date_time=date_time).all()
     result = [row.serialize for row in rows]
-    return {'code': 200, 'data': None}, result
+
+    return DEFINED_DICT['SUCCESS_CODE'], result
