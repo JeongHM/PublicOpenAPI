212dece7e375306575c6b307f2f6df1d88898dc9
JeongHaeMin <kjhm0607@gmail.com>
2020-03-23 23:04:13 +0900

    [refactor] app 설정 변경

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,20 @@
-from os import path
-from backend import create_app
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from backend.controllers.dust_controller import dust_blueprint
+from configparser import ConfigParser
 
-app, db = create_app()
+config = ConfigParser()
+config.read(os.path.abspath(os.path.join(os.path.dirname(__file__), 'config.ini')))
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+app.register_blueprint(dust_blueprint, url_prefix='/dust')
+app.config['SQLALCHEMY_DATABASE_URI'] = config['SQL_ALCHEMY']['URI']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app=app)
+
 
 @app.route('/', methods=['GET'])
 def index():
