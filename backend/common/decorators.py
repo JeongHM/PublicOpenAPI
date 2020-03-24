b96641a197cd3808d226eba865770d89b8373f0f
JeongHaeMin <kjhm0607@gmail.com>
2020-03-24 23:49:06 +0900

    [refactor] decorator 부분 service내에서 사용하도록 변경

--- backend/common/decorators.py
+++ backend/common/decorators.py
@@ -1,7 +1,1 @@
 
-def response_format_decorator(func):
-    def wrapper(*args, **kwargs):
-        res, result = func(*args, **kwargs)
-        res['data'] = result
-        return res
-    return wrapper
