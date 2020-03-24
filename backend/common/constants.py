6b025598f7376e6cec983964be73edee83219de5
JeongHaeMin <kjhm0607@gmail.com>
2020-03-24 23:48:43 +0900

    [env] constant response status code 셋팅

---
+++ backend/common/constants.py
@@ -0,0 +1,11 @@
+CONSTANTS_OBJECT = dict()
+CONSTANTS_OBJECT['STATUS_CODE'] = {
+    '200': {
+        'code': 200,
+        'message': 'success',
+    },
+    '404': {
+        'code': 404,
+        'message': 'Not Found',
+    },
+}
