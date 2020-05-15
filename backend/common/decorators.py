9464f0aaf7dbaee6a066370fe9caaa179b6acb79
JeongHaeMin <kjhm0607@gmail.com>
2020-05-15 23:45:02 +0900

    [Feat] Response 데코레이터 적용

--- backend/common/decorators.py
+++ backend/common/decorators.py
@@ -1,1 +1,10 @@
+import json
+from flask import Response
 
+
+def response_format_decorator(func):
+    def wrapper(*args, **kwargs):
+        res, result = func(*args, **kwargs)
+        res['result'] = result
+        return Response(json.dumps(res), content_type='application/json; charset=utf-8')
+    return wrapper
