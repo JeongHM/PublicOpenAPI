20f56461312c81710f6f1ea3b75cc0c212be487e
JeongHM <kjhm0607@gmail.com>
2020-12-18 01:15:59 +0900

    [feat] Setting log format
    - ADD logging

--- application.py
+++ application.py
@@ -1,15 +1,32 @@
 import os
 import logging
 
-from flask import Flask
+from flask import Flask, request, current_app
 from utils.constants import RESPONSE_CODE
 from utils.decorators import formatting_response
+from logging.handlers import RotatingFileHandler
 
 
 def create_app():
     app = Flask(import_name="Test")
 
     app.secret_key = os.urandom(16)
+
+    logger = logging.getLogger(name=__name__)
+    logger_format = "[%(process)d] [%(asctime)s] [%(filename)s] [%(funcName)s] [%(lineno)d] : %(message)s"
+    logger_formatter = logging.Formatter(fmt=logger_format)
+    logger.setLevel(level=logging.INFO)
+
+    logger_handler = RotatingFileHandler(filename="./application.log",
+                                         mode="a",
+                                         maxBytes=1024 * 1024 * 3,
+                                         backupCount=2,
+                                         encoding="utf-8")
+    logger_handler.setFormatter(fmt=logger_formatter)
+    logger.addHandler(hdlr=logger_handler)
+
+    app.logger.addHandler(logger_handler)
+    app.logger.setLevel(logging.INFO)
 
     from controllers.dust import dust_blueprint
     app.register_blueprint(blueprint=dust_blueprint, url_prefix="/dust")
@@ -18,6 +35,16 @@ def create_app():
 
 
 application = create_app()
+
+
+@application.before_request
+def before_request():
+    method = request.method
+    url = request.url
+    params = dict(request.args) if request.args else None
+    body = request.json if request.json else None
+
+    current_app.logger.info(f"[{method}] {url} params: {params} body: {body}")
 
 
 @application.route(rule="/", methods=["GET"], endpoint="health_check")
